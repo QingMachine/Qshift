c9c5be81439df4e616208cfbd5b6db9a857c0dfa
QingMachine <56369136+QingMachine@users.noreply.github.com>
2019-10-21 20:11:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include './includes/title.php'; ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Qing Machine <?= $title; ?></title>
+    <link href="styles/journey.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<header>
+    <h1>Qing</h1>
+</header>
+<div id="wrapper">
+   <?php require './includes/menu.php'; ?>
+    <main>
+        <h2>Qing helps shift awesome simple!</h2>
+        <br><br><br><br>
+        <figure>
+            <img src="images/basin.jpg" alt="Water basin at Ryoanji temple" width="350" height="237" class="picBorder">
+            <figcaption>Quite Quick!!!  Do it now!</figcaption>
+        </figure>
+        <p></p>
+        <p>Just click, edit, save as!</p>
+        <p>Bara klicka!</p>
+        <p>Klick einfach!</p>
+        <p>点就行！</p>
+        <p>Cliqez!</p>
+    </main>
+    <?php include './includes/footer.php'; ?>
+</div>
+</body>
+</html>
