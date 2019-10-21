d534e91e17bc28730a80a91942e59a6499408698
QingMachine <56369136+QingMachine@users.noreply.github.com>
2019-10-21 20:09:22 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Japan Journey</title>
+    <link href="styles/journey.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<header>
+    <h1>Japan Journey </h1>
+</header>
+<div id="wrapper">
+    <?php require './includes/menu.php'; ?>
+    <main>
+        <h2>Contact Qing  </h2>
+        <p>Om du ha vill, skicka mig.</p>
+        <form method="post" action="">
+            <p>
+                <label for="name">Name:</label>
+                <input name="name" id="name" type="text">
+            </p>
+            <p>
+                <label for="email">Email:</label>
+                <input name="email" id="email" type="text">
+            </p>
+            <p>
+                <label for="comments">Comments:</label>
+                <textarea name="comments" id="comments"></textarea>
+            </p>
+            <p>
+                <input name="send" type="submit" value="Send message">
+            </p>
+        </form>
+    </main>
+    <?php include './includes/footer.php'; ?>
+</div>
+</body>
+</html>
