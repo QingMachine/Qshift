7cf18c7a9cab076f33cc17683519d02dc6f8da37
QingMachine <56369136+QingMachine@users.noreply.github.com>
2019-10-25 13:46:59 +0200

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -4,6 +4,7 @@
         <li><a href="index.php" <?php if ($currentPage == 'index.php') { echo 'id="here"'; }?>>Home</a></li>
         <li><a href="shifter.php" <?php if ($currentPage == 'shifter.php') { echo 'id="here"'; }?>>Shifter</a></li>
         <li><a href="reporter.php" <?php if ($currentPage == 'reporter.php') { echo 'id="here"'; }?>>Reporter</a></li>
+        <li><a href="project.php" <?php if ($currentPage == 'project.php') { echo 'id="here"'; }?>>Project</a></li>
         <li><a href="contact.php" <?php if ($currentPage == 'contact.php') { echo 'id="here"'; }?>>Contact</a></li>
     </ul>
 </nav>
